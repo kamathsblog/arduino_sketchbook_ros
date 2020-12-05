fc56f03083945940ae3db6143f99f7373732f6c9
adityakamath <adityakamath@live.com>
2020-12-05 18:18:57 +0100

    updated jetbot2 arduino code

--- jetbot2_nano328o/.ipynb_checkpoints/jetbot2_nano328o-checkpoint.ino
+++ jetbot2_nano328o/.ipynb_checkpoints/jetbot2_nano328o-checkpoint.ino
@@ -11,34 +11,40 @@ github.com/adityakamath
 */
 
 #include <ros.h>
-#include <std_msgs/Float64.h>
+#include <std_msgs/Int8.h>
 #include <Adafruit_NeoPixel.h>
-
-ros::NodeHandle nh;
-std_msgs::Float64 odom_msg;
-ros::Publisher odom_pub("/odom_node/odom_linear", &odom_msg);
 
 #define LED_PIN    6
 #define LED_COUNT  8
 #define LED_BRIGHTNESS 50
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-#define TICKS_PER_ROTATION 12 //6 magnets on the shaft ring * 2 (two rising edges per magnet)
-#define TICKS_PER_10CM     18 //calibrated by testing --> define this in config file during calibration, send to arduino via serial in setup sequence
+ros::NodeHandle nh;
+std_msgs::Int8 mode_msg;
 
-volatile byte ticks;
-unsigned long time_old;
-double time_elapsed, shaft_rpm, lin_speed;
+void message_callback(const std_msgs::Int8& msg){
+    switch(msg data){
+        case 0: 
+            colorWipe(strip.Color(255, 255, 255), 10); //white
+            break;
+        case 1: 
+            colorWipe(strip.Color(255, 0, 0), 10); //red
+            break;
+        case 2: 
+            colorWipe(strip.Color(255, 0, 0), 10); //red
+            break;
+        case 3: 
+            colorWipe(strip.Color(255, 0, 0), 10); //red
+            break;
+        default:
+            colorWipe(strip.Color(0, 0, 0), 10); //off
+            break;     
+    }
+}
+
+ros::Subscriber<std_msgs::Int8> mode_sub("/joy_node/mode", &message_callback);
 
 void setup(){
-   //Serial.begin(115200);
-   attachInterrupt(0, detect_magnet, RISING);//Initialize the interrupt pin (Arduino digital pin 2)
-   
-   ticks = 0;
-   time_old = 0;
-   time_elapsed = 0;
-   shaft_rpm = 0;
-   lin_speed = 0;
 
    //setup neopixel strip
    strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
@@ -46,44 +52,16 @@ void setup(){
    strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
 
    //car bootup sequence
-   colorWipe(strip.Color(255, 0, 0), 50); //red
-   delay(50*52); //enough time for the host PC to boot up
-   colorWipe(strip.Color(0, 255, 0), 50); //green
-   delay(50*52); //enough time for the host PC to launch services
-   colorWipe(strip.Color(0, 0, 255), 50); //blue
+   mode_msg.data = 0;
+   colorWipe(strip.Color(255, 255, 255), 10); //white
    
    nh.initNode();
-   nh.advertise(odom_pub);
+   nh.subscribe(mode_sub);
 }
 
-void loop(){
-  //if(ticks >= TICKS_PER_ROTATION){ //update when 1 shaft rotation occurs
-      
-      //calculate time since last shaft rotation
-      time_elapsed = (millis() - time_old)/1000.00; //seconds
-      
-      //calculate RPM using elapsed time and number of ticks since last shaft rotation
-      shaft_rpm = (ticks*60.00) / (TICKS_PER_ROTATION*time_elapsed); //rotations per minute
-      
-      //calculate linear speed using the TICKS_PER_10CM ratio
-      lin_speed = ticks / (TICKS_PER_10CM*time_elapsed*10.00); //meters per second
-
-      //store current time and reset ticks to 0
-      time_old = millis();
-      ticks = 0;
-
-      //publish via ROS serial
-      odom_msg.data = lin_speed;
-      odom_pub.publish( &odom_msg );
-      nh.spinOnce();
-      
-      delay(1000); //update every 1 second
-  //}
-}
- 
-//ISR to increment tick value every time a rising edge is sensed by the interrupt pin
-void detect_magnet(){
-   ticks++;
+void loop(){     
+    nh.spinOnce();
+    delay(10);
 }
 
 //wipe user specified color over all 8 LEDS
