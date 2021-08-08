fd7de2e50269c7dc77b1843be4c071c186c1bdcb
adityakamath <adityakamath@live.com>
2021-08-08 05:22:16 +0200

    added akros_holo_drive

---
+++ akros_holo_drive/ros.h
@@ -0,0 +1,16 @@
+#ifndef _ROS_H_
+#define _ROS_H_
+
+#include "ros/node_handle.h"
+#include "ArduinoHardware.h"
+
+namespace ros
+{
+  // default is pubs: 25, sub: 25, in buf: 512, out buf: 512  
+  typedef NodeHandle_<ArduinoHardware, 2, 2, 160, 60> NodeHandle;
+
+  // This is legal too and will use the default 25, 25, 512, 512
+  //typedef NodeHandle_<ArduinoHardware> NodeHandle;
+}
+
+#endif
