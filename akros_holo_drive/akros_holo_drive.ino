037e19d397cefe5b745df177c478b0f5a19a6031
adityakamath <adityakamath@live.com>
2021-10-23 22:05:17 +0200

    add 10ms delay

--- akros_holo_drive/akros_holo_drive.ino
+++ akros_holo_drive/akros_holo_drive.ino
@@ -1,17 +1,15 @@
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
 #include <math.h>
-#include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
+#include <FastLED.h>
 
 #define DEBUG 0
 
 #define NEO_PIN        52
 #define NEO_COUNT      6
 #define NEO_BRIGHTNESS 50
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NEO_COUNT, NEO_PIN, NEO_GRB + NEO_KHZ800);
+
+CRGB neopixel[NEO_COUNT];
 
 #define V_MAX 255
 #define L_GAIN 1
@@ -41,29 +39,28 @@ void holonomic_drive(double x, double y, double z){
       vels[3] = V_MAX * (L_GAIN * r * sin(theta + PI/4) + A_GAIN * z) / (L_GAIN + A_GAIN); //4:rf
       vels[1] = V_MAX * (L_GAIN * r * sin(theta + PI/4) - A_GAIN * z) / (L_GAIN + A_GAIN); //2:lb
     }
+    for(int j=0; j<4; j++){
+      if(vels[j] > 0 ){
+        digitalWrite(inPins[(j+1)*2 - 2], HIGH);
+        digitalWrite(inPins[(j+1)*2 - 1], LOW);
+      }
+      else{
+        digitalWrite(inPins[(j+1)*2 - 2], LOW);
+        digitalWrite(inPins[(j+1)*2 - 1], HIGH);
+      }
+      int pwm;
+      if(abs((int)vels[j])!=0){
+        pwm = map(abs((int)vels[j]), 0, 255, MIN_PWM, 255);
+      }
+      else{
+        pwm = 0;
+      }
+      analogWrite(enPins[j], pwm);
+    }
   }
   else
   {
     drive_estop();
-  }
-
-  for(int j=0; j<4; j++){
-    if(vels[j] > 0 ){
-      digitalWrite(inPins[(j+1)*2 - 2], HIGH);
-      digitalWrite(inPins[(j+1)*2 - 1], LOW);
-    }
-    else{
-      digitalWrite(inPins[(j+1)*2 - 2], LOW);
-      digitalWrite(inPins[(j+1)*2 - 1], HIGH);
-    }
-    int pwm;
-    if(abs((int)vels[j])!=0){
-      pwm = map(abs((int)vels[j]), 0, 255, MIN_PWM, 255);
-    }
-    else{
-      pwm = 0;
-    }
-    analogWrite(enPins[j], pwm);
   }
 }
 
@@ -73,6 +70,7 @@ void drive_estop(){
   }
   for(int k=0; k<8; k++){
     digitalWrite(inPins[k], LOW);
+    digitalWrite(enPins[k], LOW);
   }
 }
 
@@ -83,41 +81,51 @@ void twist_cb(const geometry_msgs::Twist& msg)
   twist_msg = msg;
 }
 
-ros::Subscriber<geometry_msgs::Twist> twist_sub("/cmd_vel", &twist_cb);
+ros::Subscriber<geometry_msgs::Twist> twist_sub("cmd_vel", &twist_cb);
 
 void setup() {
 
-  strip.begin();
-  strip.setBrightness(NEO_BRIGHTNESS);
-  strip.show(); // Initialize all pixels to 'off'
+  FastLED.addLeds<NEOPIXEL, NEO_PIN>(neopixel, NEO_COUNT);
 
   drive_estop();
   holonomic_drive(0, 0, 0);
-  colorWipe(strip.Color(0, 255, 0), 10); // green
+  colorWipe(setLEDColor(0, 255, 0)); // green
+  FastLED.show();
 
   nh.initNode();
   nh.subscribe(twist_sub);
 }
 
 void loop() {
+  
   if(nh.connected()){
     if(DEBUG){
-      colorWipe(strip.Color(abs(twist_msg.linear.x*255), abs(twist_msg.linear.y*255), abs(twist_msg.angular.z*255)), 0);
+      colorWipe(setLEDColor(abs(twist_msg.linear.x*255), abs(twist_msg.linear.y*255), abs(twist_msg.angular.z*255)));
     }
     else{
-      colorWipe(strip.Color(0, 127, 255), 10);
+      colorWipe(setLEDColor(0, 127, 255));
     }
   }
-  
-  holonomic_drive(twist_msg.linear.x/MAX_X, twist_msg.linear.y/MAX_Y, twist_msg.angular.z/MAX_RZ);
+  else{
+    nh.spinOnce();
+  }
+
+  FastLED.show();
   nh.spinOnce();
+  holonomic_drive(twist_msg.linear.x/MAX_X, twist_msg.linear.y/MAX_Y, twist_msg.angular.z/MAX_RZ);
+  delay(10);
 }
 
-void colorWipe(uint32_t c, uint8_t wait) 
-{
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(wait);
+CRGB setLEDColor(uint8_t Rdata, uint8_t Gdata, uint8_t Bdata){
+    CRGB output(0, 0, 0);
+    output.r = Rdata;
+    output.g = Gdata;
+    output.b = Bdata;
+    return output;
+}
+
+void colorWipe(CRGB in_led){
+  for(int i=0; i<NEO_COUNT; i++) {
+    neopixel[i] = setLEDColor(in_led.r, in_led.g, in_led.b);
   }
 }
