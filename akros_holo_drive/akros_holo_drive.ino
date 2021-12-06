0e6334e1750f8c0f3d26b7780cfd13a8e35c44d6
adityakamath <adityakamath@live.com>
2021-12-06 23:32:22 +0100

    update angular velocity measurement, PWM mapping

--- akros_holo_drive/akros_holo_drive.ino
+++ akros_holo_drive/akros_holo_drive.ino
@@ -13,16 +13,14 @@
 
 CRGB neopixel[NEO_COUNT];
 
-#define V_MAX 255
-#define L_GAIN 1
-#define A_GAIN 0.5
-
-#define MAX_X 0.35
-#define MAX_Y 0.25
-#define MAX_RZ 1.85
 #define MIN_PWM 50
+#define MAX_PWM 255
+#define MAX_RPM 150
+#define MAX_RPM_GAIN 1.5
 #define ENC_CPR 300
 #define WHEEL_DIAMETER 0.075 //m
+#define WHEELS_X_DISTANCE 0.0925 //m
+#define WHEELS_Y_DISTANCE 0.225 //m
 
 //motors 1:lf, 2:lb, 3:rb, 4:rf
 Encoder enc1(19, 17);
@@ -33,19 +31,30 @@ int enPins[4] = {8, 9, 10, 11};
 int inPins[8] = {32, 34, 36, 38, 42, 40, 46, 44};
 double vels[4] = {0, 0, 0, 0};
 
-void holonomic_drive(double x, double y, double z){
-  
-  double theta = atan2(y, x);
-  double r     = sqrt((pow(x, 2) + pow(y, 2)));
+void holonomic_drive(double x, double y, double a){
 
-  if(x!=0 || y!=0 || z!=0)
-  {
-    {
-      vels[0] = V_MAX * (L_GAIN * r * cos(theta + PI/4) - A_GAIN * z) / (L_GAIN + A_GAIN); //1:lf
-      vels[2] = V_MAX * (L_GAIN * r * cos(theta + PI/4) + A_GAIN * z) / (L_GAIN + A_GAIN); //3:rb
-      vels[3] = V_MAX * (L_GAIN * r * sin(theta + PI/4) + A_GAIN * z) / (L_GAIN + A_GAIN); //4:rf
-      vels[1] = V_MAX * (L_GAIN * r * sin(theta + PI/4) - A_GAIN * z) / (L_GAIN + A_GAIN); //2:lb
-    }
+  float tangential = a * ((WHEELS_X_DISTANCE / 2) + (WHEELS_Y_DISTANCE / 2)); // m/s
+
+  float x_rpm = constrain(x * 60 / (PI * WHEEL_DIAMETER), -MAX_RPM, MAX_RPM); // rotation per minute
+  float y_rpm = constrain(y * 60 / (PI * WHEEL_DIAMETER), -MAX_RPM, MAX_RPM); // rotation per minute
+  float a_rpm = constrain(tangential * 60 / (PI * WHEEL_DIAMETER), -MAX_RPM, MAX_RPM); // rotation per minute
+
+  /*
+  Serial.print("Provided X: ");
+  Serial.println(x_rpm);
+  Serial.print("Provided Y: ");
+  Serial.println(y_rpm);
+  Serial.print("Provided A: ");
+  Serial.println(a_rpm);
+  */
+
+  if(x!=0 || y!=0 || a!=0){
+    vels[0] = x_rpm - y_rpm - a_rpm; //1:lf
+    vels[1] = x_rpm + y_rpm - a_rpm; //2:lb
+    vels[2] = x_rpm - y_rpm + a_rpm; //3:rb
+    vels[3] = x_rpm + y_rpm + a_rpm; //4:rf
+
+    //instead of mapping it directly, use PID to determine RPM->PWM conversion, 
     for(int j=0; j<4; j++){
       if(vels[j] > 0 ){
         digitalWrite(inPins[(j+1)*2 - 2], HIGH);
@@ -57,7 +66,7 @@ void holonomic_drive(double x, double y, double z){
       }
       int pwm;
       if(abs((int)vels[j])!=0){
-        pwm = map(abs((int)vels[j]), 0, 255, MIN_PWM, 255);
+        pwm = map(abs((int)vels[j]), 0, MAX_RPM_GAIN*MAX_RPM, MIN_PWM, MAX_PWM);
       }
       else{
         pwm = 0;
@@ -65,8 +74,7 @@ void holonomic_drive(double x, double y, double z){
       analogWrite(enPins[j], pwm);
     }
   }
-  else
-  {
+  else{
     drive_estop();
   }
 }
@@ -87,13 +95,11 @@ ros::Subscriber<geometry_msgs::Twist> twist_sub("cmd_vel", &twist_cb);
 geometry_msgs::Point raw_vel_msg;
 ros::Publisher raw_vel_pub("raw_vel", &raw_vel_msg);
 
-void twist_cb(const geometry_msgs::Twist& msg)
-{
+void twist_cb(const geometry_msgs::Twist& msg){
   twist_msg = msg;
 }
 
 void setup() {
-
   FastLED.addLeds<NEOPIXEL, NEO_PIN>(neopixel, NEO_COUNT);
 
   drive_estop();
@@ -108,24 +114,34 @@ void setup() {
 }
 
 void loop() {
-  
-  int vel1, vel2, vel3, vel4;
-  vel1 = enc1.readRPM(ENC_CPR);
-  vel2 = enc2.readRPM(ENC_CPR);
-  vel3 = -1 * enc3.readRPM(ENC_CPR);
-  vel4 = -1 * enc4.readRPM(ENC_CPR);
+  double enc_vels[4] = {0, 0, 0, 0};
 
-  float average_rps_x = ((vel1 + vel2 + vel3 + vel4)/4)/60.0;
-  raw_vel_msg.x = average_rps_x * PI * WHEEL_DIAMETER; // m/s
+  enc_vels[0] = enc1.readRPM(ENC_CPR);  //1:lf
+  enc_vels[1] = enc2.readRPM(ENC_CPR);  //2:lb
+  enc_vels[2] = -enc3.readRPM(ENC_CPR); //3:rb - reversed polarity
+  enc_vels[3] = -enc4.readRPM(ENC_CPR); //4:rf - reversed polarity
 
-  float average_rps_y = ((vel2 + vel4 - vel1 - vel3)/4)/60.0;
-  raw_vel_msg.y = average_rps_y * PI * WHEEL_DIAMETER; // m/s
+  float avg_rpm_x = ((enc_vels[0] + enc_vels[1] + enc_vels[2] + enc_vels[3])/4);
+  raw_vel_msg.x = avg_rpm_x * PI * WHEEL_DIAMETER / 60; // m/s
 
-  float average_rps_a = ((vel3 + vel4 - vel1 - vel2)/4)/60.0;
-  raw_vel_msg.z = average_rps_a * PI * WHEEL_DIAMETER; // m/s
+  float avg_rpm_y = ((enc_vels[1] + enc_vels[3] - enc_vels[0] - enc_vels[2])/4);
+  raw_vel_msg.y = avg_rpm_y * PI * WHEEL_DIAMETER / 60; // m/s
+
+  float avg_rpm_a = ((enc_vels[2] + enc_vels[3] - enc_vels[0] - enc_vels[1])/4);
+  raw_vel_msg.z = avg_rpm_a * PI * WHEEL_DIAMETER / ((WHEELS_X_DISTANCE/2 + WHEELS_Y_DISTANCE/2)*60); // rad/s
 
   raw_vel_pub.publish(&raw_vel_msg);
-  holonomic_drive(twist_msg.linear.x/MAX_X, twist_msg.linear.y/MAX_Y, twist_msg.angular.z/MAX_RZ);
+  holonomic_drive(twist_msg.linear.x, twist_msg.linear.y, twist_msg.angular.z);
+  
+  /*
+  holonomic_drive(0, 0, 0.5);
+  Serial.print("measured X: ");
+  Serial.println(raw_vel_msg.x);
+  Serial.print("measured Y: ");
+  Serial.println(raw_vel_msg.y);
+  Serial.print("measured A: ");
+  Serial.println(raw_vel_msg.z * ((WHEELS_X_DISTANCE/2 + WHEELS_Y_DISTANCE/2)*60));
+  */
 
   if(nh.connected()){
     if(DEBUG){ colorWipe(setLEDColor(abs(twist_msg.linear.x*255), abs(twist_msg.linear.y*255), abs(twist_msg.angular.z*255))); }
@@ -134,7 +150,7 @@ void loop() {
   FastLED.show();
   
   nh.spinOnce();
-  delay(20);
+  delay(10);
 }
 
 CRGB setLEDColor(uint8_t Rdata, uint8_t Gdata, uint8_t Bdata){
