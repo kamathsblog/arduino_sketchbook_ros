2da1c78111f0f8d540d4302bf638075cae67127d
adityakamath <adityakamath@live.com>
2022-06-04 23:18:03 +0200

    added akros_teensy_pio

---
+++ akros_teensy_pio/src/akros_holo_teensy_config.h
@@ -0,0 +1,62 @@
+//DEBUG
+#define DEBUG 0
+#define OPEN_LOOP 0
+
+//PID
+#define KP 0.75
+#define KI 0.50
+#define KD 0.50
+#define UPDATE_RATE 20 //ms
+
+//NEOPIXEL
+#define NEO_PIN        24 //52
+#define NEO_COUNT      6
+#define NEO_BRIGHTNESS 50
+
+//MODES
+#define ESTOP 0
+#define AUTO_T 1
+#define PLAY_WP 2
+#define PLAY_T 3
+#define RECORD 4
+
+//MOTOR CONTROL
+#define MIN_PWM 60
+#define MAX_PWM 245
+#define MAX_RPM 150
+#define OPEN_LOOP_GAIN 1.5
+#define ENC_CPR 296
+#define WHEEL_DIAMETER 0.0762 //m 3"
+#define WHEELS_X_DISTANCE 0.093 //m
+#define WHEELS_Y_DISTANCE 0.210 //m
+
+#define SCALE_X 0.30
+#define SCALE_Y 0.25
+#define SCALE_RZ 1.57
+
+//MOTOR CONTROL WIRING
+//1:LF, 2:LB, 3:RB, 4:RF
+
+//MOTOR/ENCODER 1: FRONT LEFT
+#define M1_a 2  //37
+#define M1_b 3  //36
+#define E1_a 31 //19
+#define E1_b 29 //17
+
+//MOTOR/ENCODER 2: BACK LEFT
+#define M2_a 36 //39
+#define M2_b 37 //38
+#define E2_a 30 //18
+#define E2_b 28 //16
+
+//MOTOR/ENCODER 3: BACK RIGHT
+#define M3_a 4  //40
+#define M3_b 5  //41
+#define E3_a 32 //20
+#define E3_b 9  //24
+
+//MOTOR/ENCODER 4: FRONT RIGHT
+#define M4_a 6  //42
+#define M4_b 7  //43
+#define E4_a 33 //21
+#define E4_b 8  //22
--- akros_teensy_pio/src/main.cpp
+++ akros_teensy_pio/src/main.cpp
@@ -3,7 +3,8 @@
 #include <geometry_msgs/Point.h>
 #include <Encoder.h>
 #include <FastLED.h>
-#include "akros_holo_config.h"
+//#include <CytronMotorDriver.h>
+#include "akros_holo_teensy_config.h"
 
 //Global variables and objects
 geometry_msgs::Point twist_msg;
@@ -17,8 +18,11 @@ double prev_error[4] = {0, 0, 0, 0};
 double integral[4]   = {0, 0, 0, 0};
 double derivative[4] = {0, 0, 0, 0};
 
-int enPins[4] = {M1_en, M2_en, M3_en, M4_en};
 int inPins[8] = {M1_a, M1_b, M2_a, M2_b, M3_a, M3_b, M4_a, M4_b};
+// CytronMD motor1(M1_a, M1_b);
+// CytronMD motor2(M2_a, M2_b);
+// CytronMD motor3(M3_a, M3_b);
+// CytronMD motor4(M4_a, M4_b);
 
 Encoder enc1(E1_a, E1_b);
 Encoder enc2(E2_a, E2_b);
@@ -55,15 +59,16 @@ ros::Subscriber<geometry_msgs::Point> twist_sub("cmd_vel/vector", &twist_cb);
 ros::Subscriber<std_msgs::UInt32> mode_sub("cmd_vel/mode", &mode_cb);
 ros::Publisher raw_vel_pub("raw_vec", &raw_vel_msg);
 
-void setup() {
+void setup()
+{
   //Startup LEDs - green
   FastLED.addLeds<NEOPIXEL, NEO_PIN>(neopixel, NEO_COUNT);
   colorWipe(setLEDColor(0, MAX_PWM, 0)); // green
   FastLED.show();
-  
+
   //Startup Drive - set all pins to 0
   drive_estop();
-  
+
   //Startup ROS - initialize node, subscribe and advertise to topics
   nh.initNode();
   nh.subscribe(twist_sub);
@@ -71,70 +76,87 @@ void setup() {
   nh.advertise(raw_vel_pub);
 
   //Connect to ROS
-  while(!nh.connected()){ nh.spinOnce(); }
-  
+  while(!nh.connected())
+  {
+    nh.spinOnce();
+  }
+
   //ROS connected
   colorWipe(setLEDColor(0, MAX_PWM/2, MAX_PWM)); //blue
   FastLED.show();
 }
 
-void loop() {
-  if(mode_msg[ESTOP] == true){ // STOP! - red
+void loop()
+{
+  if(mode_msg[ESTOP] == true)
+  { // STOP! - red
     drive_estop();
     colorWipe(setLEDColor(255, 0, 0));
-    
+
     holonomic_drive(0, 0, 0);
-    nh.spinOnce();  
+    nh.spinOnce();
   }
   else{
-    if(mode_msg[AUTO_T] == true){ // AUTO
-      if(mode_msg[PLAY_WP] == true){ // AUTO > PLAYBACK WAYPOINTS - pink 0xe6007e
+    if(mode_msg[AUTO_T] == true)
+    { // AUTO
+      if(mode_msg[PLAY_WP] == true)
+      { // AUTO > PLAYBACK WAYPOINTS - pink 0xe6007e
         colorWipe(setLEDColor(230, 0, 126));
       }
-      else if(mode_msg[PLAY_T] == true){ // AUTO > PLAYBACK TRAJECTORY - green 0x70ff00
+      else if(mode_msg[PLAY_T] == true)
+      { // AUTO > PLAYBACK TRAJECTORY - green 0x70ff00
         colorWipe(setLEDColor(112, 255, 0));
       }
-      else{ // AUTO > NORMAL -- blue 0x0070ff
-        colorWipe(setLEDColor(0, 112, 255)); 
+      else
+      { // AUTO > NORMAL -- blue 0x0070ff
+        colorWipe(setLEDColor(0, 112, 255));
       }
     }
-    else{ // TELEOP
-      if(mode_msg[RECORD] == true){ // TELEOP > RECORD - Orange Red #ff4500
+    else
+    { // TELEOP
+      if(mode_msg[RECORD] == true)
+      { // TELEOP > RECORD - Orange Red #ff4500
         colorWipe(setLEDColor(255, 69, 0));
       }
-      else{ // TELEOP > NORMAL - rgb according to xyz or blueish-white #f5f5ff
-        if(DEBUG){colorWipe(setLEDColor(abs(twist_msg.x*MAX_PWM/SCALE_X), abs(twist_msg.y*MAX_PWM/SCALE_Y), abs(twist_msg.z*MAX_PWM/SCALE_RZ)));}
-        else{ 
-          colorWipe(setLEDColor(245, 245, 255)); 
+      else
+      { // TELEOP > NORMAL - rgb according to xyz or blueish-white #f5f5ff
+        if(DEBUG)
+        {
+          (setLEDColor(abs(twist_msg.x*MAX_PWM/SCALE_X), abs(twist_msg.y*MAX_PWM/SCALE_Y), abs(twist_msg.z*MAX_PWM/SCALE_RZ)));
+        }
+        else
+        {
+          colorWipe(setLEDColor(245, 245, 255));
         }
       }
     }
-    
+
     //Loop Drive - drive based on global twist message values
     holonomic_drive(twist_msg.x, twist_msg.y, twist_msg.z);
     nh.spinOnce();
   }
-  
+
   FastLED.show();
-      
+
   //Loop Publisher - compute velocities from holonomic_drive and publish
   float avg_rpm_x = ((rpm_meas[0] + rpm_meas[1] + rpm_meas[2] + rpm_meas[3])/4);
   float avg_rpm_y = ((rpm_meas[1] + rpm_meas[3] - rpm_meas[0] - rpm_meas[2])/4);
   float avg_rpm_a = ((rpm_meas[2] + rpm_meas[3] - rpm_meas[0] - rpm_meas[1])/4);
-  
+
   raw_vel_msg.x = avg_rpm_x * PI * WHEEL_DIAMETER / 60; // m/s
   raw_vel_msg.y = avg_rpm_y * PI * WHEEL_DIAMETER / 60; // m/s
   raw_vel_msg.z = avg_rpm_a * PI * WHEEL_DIAMETER / ((WHEELS_X_DISTANCE/2 + WHEELS_Y_DISTANCE/2)*60); // rad/s
 
   raw_vel_pub.publish(&raw_vel_msg);
   nh.spinOnce();
-      
+
   //loop at 50Hz frequency
   delay(20);
 }
 
 //PID Controller: Calculates output PWM for a motor using its reference and measured speeds
-void pid(int motor, double reference, double measurement, double kp, double ki, double kd){
+void pid(int motor, double reference, double measurement, double kp, double ki, double kd)
+{
   double error = reference - measurement;
   integral[motor] += error;
   derivative[motor] += error - prev_error[motor];
@@ -146,41 +168,62 @@ void pid(int motor, double reference, double measurement, double kp, double ki, 
 }
 
 //Writes digital/PWM values to motor direction/enable pins for the specified motor
-void spin_motor(int motor, double velocity){
-  if(velocity > 0 ){
-    digitalWrite(inPins[2*motor], HIGH);
-    digitalWrite(inPins[2*motor + 1], LOW);
+void spin_motor(int motor, double velocity)
+{
+  int motor_pwm = constrain(abs((int)velocity), MIN_PWM, MAX_PWM);
+  // switch (motor)
+  // {
+  //   case 0:
+  //     motor1.setSpeed(motor_pwm);
+  //     break;
+  //   case 1:
+  //     motor2.setSpeed(motor_pwm);
+  //     break;
+  //   case 2:
+  //     motor3.setSpeed(motor_pwm);
+  //     break;
+  //   case 3:
+  //     motor4.setSpeed(motor_pwm);
+  //     break;
+  //   default:
+  //     break;
+  // }
+  if(velocity >= 0)
+  {
+    analogWrite(inPins[2*motor], motor_pwm);
+    analogWrite(inPins[2*motor + 1], 0);
   }
-  else{
-    digitalWrite(inPins[2*motor], LOW);
-    digitalWrite(inPins[2*motor + 1], HIGH);
+  else
+  {
+    analogWrite(inPins[2*motor], 0);
+    analogWrite(inPins[2*motor + 1], motor_pwm);
   }
-  int motor_pwm = constrain(abs((int)velocity), MIN_PWM, MAX_PWM);  
-  analogWrite(enPins[motor], motor_pwm);   
 }
 
 //Using holonomic drive kinematics, drives individual motors based on input linear/angular velocities
-void holonomic_drive(double x, double y, double a){
-
+void holonomic_drive(double x, double y, double a)
+{
   float tangential = a * ((WHEELS_X_DISTANCE / 2) + (WHEELS_Y_DISTANCE / 2)); // m/s
   float x_rpm = constrain(x * 60 / (PI * WHEEL_DIAMETER), -MAX_RPM, MAX_RPM); // rotation per minute
   float y_rpm = constrain(y * 60 / (PI * WHEEL_DIAMETER), -MAX_RPM, MAX_RPM); // rotation per minute
   float a_rpm = constrain(tangential * 60 / (PI * WHEEL_DIAMETER), -MAX_RPM, MAX_RPM); // rotation per minute
 
-  if(x!=0 || y!=0 || a!=0){
+  if(x!=0 || y!=0 || a!=0)
+  {
     rpm_ref[0] = x_rpm - y_rpm - a_rpm; //1:lf
     rpm_ref[1] = x_rpm + y_rpm - a_rpm; //2:lb
     rpm_ref[2] = x_rpm - y_rpm + a_rpm; //3:rb
     rpm_ref[3] = x_rpm + y_rpm + a_rpm; //4:rf
- 
-    for(int j=0; j<4; j++){
+
+    for(int j=0; j<4; j++)
+    {
       pid(j, rpm_ref[j], rpm_meas[j], KP, KI, KD);
-        
       double motor_speed = OPEN_LOOP ? OPEN_LOOP_GAIN*rpm_ref[j] : pwm_out[j];
       spin_motor(j, motor_speed);
     }
   }
-  else{
+  else
+  {
     drive_estop();
   }
 
@@ -190,16 +233,24 @@ void holonomic_drive(double x, double y, double a){
   rpm_meas[3] = -enc4.readRPM(ENC_CPR); //4:rf - reversed polarity
 }
 
-//Hard stops all motors - sets integral value, reference velocities, pwm_values to zero
-void drive_estop(){
-  for(int i=0; i<4; i++){
+//Hard stops all motors - reference velocities, motor pins to zero
+void drive_estop()
+{
+  for(int i=0; i<4; i++)
+  {
       rpm_ref[i] = 0;
-      digitalWrite(enPins[i], LOW);
+      analogWrite(inPins[2*i], 0);
+      analogWrite(inPins[2*i+1], 0);
   }
+  // motor1.setSpeed(0);
+  // motor2.setSpeed(0);
+  // motor3.setSpeed(0);
+  // motor4.setSpeed(0);
 }
 
 //Returns RGB values in CRGB format
-CRGB setLEDColor(uint8_t Rdata, uint8_t Gdata, uint8_t Bdata){
+CRGB setLEDColor(uint8_t Rdata, uint8_t Gdata, uint8_t Bdata)
+{
     CRGB output(0, 0, 0);
     output.r = Rdata;
     output.g = Gdata;
@@ -208,8 +259,10 @@ CRGB setLEDColor(uint8_t Rdata, uint8_t Gdata, uint8_t Bdata){
 }
 
 //Sets all LEDs to CRGB color in sequence
-void colorWipe(CRGB in_led){
-  for(int i=0; i<NEO_COUNT; i++) {
+void colorWipe(CRGB in_led)
+{
+  for(int i=0; i<NEO_COUNT; i++)
+  {
     neopixel[i] = setLEDColor(in_led.r, in_led.g, in_led.b);
   }
 }
